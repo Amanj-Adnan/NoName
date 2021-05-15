6e9fb61abb93d818223cff527e642700e20c0f35
amanj adnan <amanjadnan7@gmail.com>
2021-05-15 10:39:43 +0300

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profile' , to: "user#show"
   get 'test/show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
