7a6c20b8b2ea5be4d483f97aac34fa6f00ad051a
amanj adnan <amanjadnan7@gmail.com>
2021-05-17 16:10:35 +0300

    SetUp Rregistration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'profile' , to: "user#show"
-  get 'test/show'
+  root  "home#show"
+  get "sign_up" , to: "register#create"
+  post "sign_up" , to: "register#created"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('@tailwindcss/forms'),
     require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
     require('autoprefixer'),
     require('postcss-import'),
