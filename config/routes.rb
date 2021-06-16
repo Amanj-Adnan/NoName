1c0101c85e3f1baf4c2a94d668b876c673bda2da
amanj adnan <amanjadnan7@gmail.com>
2021-06-16 11:49:33 +0300

    update ui

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   get "/auth/twitter/callback" , to: "omniauth_callback#twitter"
 
+  resources :twitter_accounts
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
